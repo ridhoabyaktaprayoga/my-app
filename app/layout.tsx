0a88e2307116eb3811d50cd2b30a088908c59528
ridhoabyaktaprayoga <ridhoabiyaktaprayoga13@gmail.com>
2024-08-08 08:28:04 +0700

    feat: components,navbar,container

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from './components/Navbar/navbar';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +15,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
+  return ( 
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
